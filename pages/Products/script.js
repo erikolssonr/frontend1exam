793c803bb2ec64d8dd3054e563ebc7248b25ac02
Erik Olsson Rhann <Erikolssonr@gmail.com>
2023-12-17 01:08:23 +0100

    fixade total cost i kundvagn med js

--- pages/Products/script.js
+++ pages/Products/script.js
@@ -4,8 +4,6 @@ let body = document.querySelector('body');
 let increaseBtn = document.querySelector('.plus');
 let decreaseBtn = document.querySelector('.minus');
 
-
-
 function incrementBtnOnClick(e){
     let incrementBtnElement = e.target;
     incrementAmount(incrementBtnElement)
@@ -35,7 +33,10 @@ function incrementAmount(element){
         let newValue = amount;
         newValue++;
         amountSpan.innerHTML = newValue;
+        calcTotalCost()
     }
+
+    
 }
 
 function decrementAmount(e){
@@ -48,9 +49,49 @@ function decrementAmount(e){
         let newValue = amount;
         newValue--;
         amountSpan.innerHTML = newValue;
+        calcTotalCost()
     }
 }
 
+function calcTotalCostOfItem(element, amount) {
+    let parent = element.parentElement
+    let totalPriceElement;
+    let totalPriceString;
+    let productPrice;
+    
+    for (let sibling of parent.children){
+        if (sibling.classList.contains('totalPrice')){
+            totalPriceElement = sibling;
+            totalPriceString = totalPriceElement.innerHTML.replace(':', '').replace('-', '').trim();
+            productPrice = parseInt(totalPriceString);
+            totalCost = productPrice * amount;
+            return totalCost;
+        }
+    }
+}
+
+function calcTotalCost() {
+    let listCartElement = document.getElementById('listCart')
+    let totalCost = 0;
+
+    for (let listCartItem of listCartElement.children){
+        let listCartItemQuantityElement;
+        
+        for (let listCartItemChild of listCartItem.children){
+            
+            if (listCartItemChild.classList.contains('quantity')) {
+                listCartItemQuantityElement = listCartItemChild
+                totalCost += calcTotalCostOfItem(listCartItemQuantityElement, listCartItemQuantityElement.querySelector('.amount').innerHTML)
+            }
+        }
+    }
+    updateTotalCost(totalCost)
+}
+
+function updateTotalCost(totalCost){
+    let totalAmountElement = document.querySelector('.totalAmount');
+    totalAmountElement.innerHTML = totalCost.toFixed(2) + ' SEK';
+}
 
 iconCart.addEventListener('click', () => {
     body.classList.toggle('showCart');
@@ -59,4 +100,5 @@ iconCart.addEventListener('click', () => {
 closeCart.addEventListener('click', () => {
     body.classList.toggle('showCart');
 });
+
 
